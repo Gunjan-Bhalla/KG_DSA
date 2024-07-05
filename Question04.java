b6c998106194164c45a569be7fe9cb4733fd893d
Gunjan <87493284+Gunjan-Bhalla@users.noreply.github.com>
2024-07-05 11:37:29 +0000

    patterns

---
+++ Question04.java
@@ -0,0 +1,12 @@
+public class Question04 {
+    public static void main(String args[]){
+        System.out.println("Right Half Pyramid");
+        System.out.println(" *\n * *\n * * *\n * * * *\n * * * * *");
+
+        System.out.println("Reverse Right Half Pyramid");
+        System.out.println(" * * * * *\n * * * *\n * * *\n * *\n *");
+
+        System.out.println("Left Hand Pyramid");
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
